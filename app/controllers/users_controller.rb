71080c7fb86e7f2600cd27e8d6aea30ccc2c8db5
uko-h <hourglassu@yahoo.co.jp>
2019-12-22 16:37:03 +0900

    incremental_search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
 
+  def index
+    return nil if params[:kyword] == ""
+    @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"]).where.not(id: current_user.id).limit(10)
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
   def edit
   end
 
